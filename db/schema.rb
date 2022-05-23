2251cc2862c123225ae468caf9c5c0cd2f0f98de
clement spiers <clement.spiers@gmail.com>
2022-05-23 17:48:48 +0200

    creation of the equipment and the booking models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_151008) do
+ActiveRecord::Schema.define(version: 2022_05_23_154611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,34 @@ ActiveRecord::Schema.define(version: 2022_05_23_151008) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "equipment_id", null: false
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.string "location"
+    t.integer "total_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["equipment_id"], name: "index_bookings_on_equipment_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "equipment", force: :cascade do |t|
+    t.string "sport"
+    t.string "brand"
+    t.bigint "user_id", null: false
+    t.integer "price"
+    t.string "image"
+    t.string "description"
+    t.integer "board_size"
+    t.integer "sail_size"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_equipment_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -62,4 +90,7 @@ ActiveRecord::Schema.define(version: 2022_05_23_151008) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "equipment"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "equipment", "users"
 end
