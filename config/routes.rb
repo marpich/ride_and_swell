b726fa30d00f0f22e407038aa54418aac82f0140
Margaux Pichard <margauxpichard@gmail.com>
2022-05-23 17:24:54 +0200

    finalizing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :equipments, only:[:new, :create, :show, :index] do
-    resources :bookings, only:[:create]
+
+  namespace :owner do
+    resources :equipments, only: [:new, :create]
+    resources :bookings, except: [:show, :index, :new, :create, :destroy, :update, :edit] do
+      member do
+        post :reject
+        post :accept
+      end
+    end
   end
+
+  resources :equipments, only: [:show, :index] do
+    resources :bookings, only: [:create]
+  end
+
+  get "/dashboard", to: "users#dashboard"
+
+  # post "/owner/bookings/:id/reject", to: "bookings#reject"
+  # post "/owner/bookings/:id/accept", to: "bookings#accept"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
