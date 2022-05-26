95e9b98f8a3818993fefda108da48418c14ed6e0
Cédric le Brun <lebruncedric8@gmail.com>
2022-05-26 13:39:18 +0200

    validation booking en cours mais statut ne change pas d'affichage

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -2,12 +2,10 @@ class Owner::BookingsController < ApplicationController
   def accept
     @booking = Booking.find(params[:id])
     @booking.status = t(:accepted)
-    raise
   end
 
   def reject
     @booking = Booking.find(params[:id])
     @booking.status = t(:rejected)
-    raise
   end
 end
